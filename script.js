5defaf2bb5722f7853bddc7a0acb35d1f255962f
DivyaRajesh07 <divyarajesh0710@gmail.com>
2026-09-01 15:50:45 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,313 @@
+/*
+TASK-01(VAR,LET,CONST)
+======================
+
+var studentName = "Divya";
+let studentAge = 23;
+const collegeName = "Arunai College";
+
+console.log(studentName);
+console.log(studentAge);
+console.log(collegeName);
+
+studentName = "Rajesh";
+studentAge = 23;
+
+console.log(studentName);
+console.log(studentAge);
+
+collegeName = "XYZ College";*/
+
+/*
+TASK-02(USER INFORMAYTION)
+==========================
+
+let name = prompt("Enter your name:");
+let age = prompt("Enter your age:");
+let city = prompt("Enter your city:");
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("City:", city);*/
+
+/*
+TASK-03(WELCOME MESSAGE)
+=========================
+
+let name = prompt("Enter your name:");
+
+alert("Welcome " + name + "!");*/
+
+/*
+TASK-04(AGE CALCULATION)
+========================
+
+let birthYear = prompt("Enter your birth year:");
+
+let age = 2026 - birthYear;
+
+console.log("Birth Year:", birthYear);
+console.log("Age:", age);*/
+
+
+/*
+DATA TYPES TASK
+================
+TASK-05(IDENTIFY DATA TYPES)
+============================
+
+let name = "Rajesh";
+let age = 27;
+let isStudent = true;
+let city;
+let value = null;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof city);
+console.log(typeof value);*/
+
+
+/*
+TASK-06(STUDENT DATA)
+=====================
+let student = {
+    name: "Divya",
+    age: 24,
+    city: "cuddalore",
+    qualification: "MSC",
+    isStudent: true
+};
+
+console.log(student);
+console.log(student.name);
+console.log(student.age);
+console.log(student.qualification);
+console.log(student.isStudent);*/
+
+
+/*
+TASK-07(FRUIT ARRAY)
+====================
+
+let fruits = [
+    "Apple",
+    "Mango",
+    "Orange",
+    "Banana",
+    "Grapes",
+    "Papaya"
+];
+
+console.log("First fruit:", fruits[0]);
+console.log("Second fruit:", fruits[1]);
+console.log("Last fruit:", fruits[fruits.length - 1]);
+console.log("Total fruits:", fruits.length);*/
+
+/*
+ARITHMETIC OPERATOR TASKS
+=========================
+TASK-08(BASIC CALCULATOR)
+=========================
+let a = 20;
+let b = 5;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
+console.log("Exponentiation:", a ** b);*/
+
+
+/*
+TASK-09(SHOPPING BILL)
+======================
+
+let shirt = 999;
+let pant = 1499;
+let shoes = 1999;
+
+let total = shirt + pant + shoes;
+
+console.log("Total:", total);*/
+
+
+/*
+TASK-10(SIMPLE MARKS CALC)
+==========================
+
+let tamil = 80;
+let english = 75;
+let maths = 90;
+
+let total = tamil + english + maths;
+let average = total / 3;
+
+console.log("Total Marks:", total);
+console.log("Average Marks:", average);*/
+
+
+/*
+INCREMENT AND DECREMENT TASKS:
+==============================
+TASK-11(POST INCREMENT)
+========================
+
+let a = 10;
+
+let b = a++;
+
+console.log(a);
+console.log(b);*/
+
+
+/*
+TASK-12(PRE INCREMENT)
+======================
+
+let a = 10;
+
+let b = ++a;
+
+console.log(a);
+console.log(b);*/
+
+
+/*
+TASK-13(POST DECREMENT)
+=======================
+let a = 20;
+
+let b = a--;
+
+console.log(a);
+console.log(b);*/
+
+/*
+TASK-14(PRE DECREMENT)
+======================
+
+let a = 20;
+
+let b = --a;
+
+console.log(a);
+console.log(b);*/
+
+
+/*
+TASK-15(CHALLENGE TASK)
+=======================
+
+let a = 5;
+
+let b = a++;
+
+let c = ++a;
+
+let d = b--;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);*/
+
+
+/*
+TASK-16(ASSIGNMENT OPERATORS)
+==============================
++=
+====
+let num = 10;
+
+num += 5;
+
+console.log(num);
+-=
+====
+let num = 10;
+
+num -= 5;
+
+console.log(num);
+*=
+=====
+let num = 10;
+
+num *= 5;
+
+console.log(num);
+
+/=
+====
+let num = 10;
+
+num /= 5;
+
+console.log(num);
+
+%=
+=====
+let num = 10;
+
+num %= 5;
+
+console.log(num);
+
+**=
+=====
+let num = 10;
+
+num **= 5;
+
+console.log(num);*/
+
+
+/*
+TASK-17 Mini Student Profile
+=============================
+
+let name = "Divya";
+let age = 24;
+let city = "cuddalore";
+let college = "Arunai College";
+
+let subjects = [
+    "Tamil",
+    "English",
+    "Maths",
+    "Science",
+    "Computer"
+];
+
+let student = {
+    name: name,
+    age: age,
+    city: city,
+    subjects: subjects,
+    isStudent: true
+};
+
+console.log("Student Name:", student.name);
+console.log("Student Age:", student.age);
+console.log("City:", student.city);
+console.log("First Subject:", subjects[0]);
+console.log("Last Subject:", subjects[subjects.length - 1]);
+console.log("Total Subjects:", subjects.length);
+console.log("Complete Object:", student);*/
+
+/*
+TASK-18 Final Challenge — User + Calculator
+=============================================
+
+let number1 = prompt("Enter first number:");
+let number2 = prompt("Enter second number:");
+
+console.log("Addition:", number1 + number2);
+console.log("Subtraction:", number1 - number2);
+console.log("Multiplication:", number1 * number2);
+console.log("Division:", number1 / number2);
+console.log("Modulus:", number1 % number2);
+console.log("Power:", number1 ** number2);*/
